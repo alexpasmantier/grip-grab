0daee4e6fb7993cbb61b26a9dbbefee456458b6a
alexpasmantier <alex.pasmant@gmail.com>
2024-09-01 01:55:31 +0200

    refactor: fallback to regular search when readable stdin is empty

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::{stdin, Read};
 use std::path::PathBuf;
 use std::sync::Arc;
 
@@ -24,83 +25,86 @@ mod utils;
 pub fn main() -> anyhow::Result<()> {
     let cli_args = process_cli_args(Cli::parse())?;
 
-    let stdin = std::io::stdin();
     if is_readable_stdin() {
-        let matcher = build_matcher(&cli_args.patterns)?;
-        let mut searcher = build_searcher(cli_args.multiline);
-        match search_reader(stdin.lock(), &matcher, &mut searcher) {
-            Ok(search_results) => {
-                let printer_config = PrinterConfig {
-                    mode: cli_args.print_mode,
-                    absolute_paths: cli_args.absolute_paths,
-                    colored_output: cli_args.colored_output,
-                    disable_hyperlinks: cli_args.disable_hyperlinks,
-                    ..Default::default()
-                };
-                let mut printer = Printer::new(printer_config);
-                printer.write(FileResults {
-                    path: PathBuf::from("stdin"),
-                    results: search_results,
-                })?;
-                printer.print()?;
-            }
-            Err(err) => {
-                eprintln!("Error: {}", err);
-            }
-        }
-        Ok(())
-    } else {
-        let queue: Arc<SegQueue<FileResults>> = Arc::new(SegQueue::new());
-
-        let haystack_builder = walk_builder(
-            cli_args.paths.iter().map(|p| p.as_path()).collect(),
-            &cli_args.ignored_paths,
-            cli_args.n_threads,
-            !cli_args.disregard_gitignore,
-            cli_args.filter_filetypes,
-        );
-        let matcher: Arc<RegexMatcher> = Arc::new(build_matcher(&cli_args.patterns)?);
-        haystack_builder.build_parallel().run(|| {
-            let matcher = Arc::clone(&matcher);
+        let stdin = stdin();
+        let mut buf = Vec::new();
+        if stdin.lock().read_to_end(&mut buf)? != 0 {
+            let matcher = build_matcher(&cli_args.patterns)?;
             let mut searcher = build_searcher(cli_args.multiline);
-            let queue = Arc::clone(&queue);
-            Box::new(move |entry: Result<DirEntry, ignore::Error>| match entry {
-                Ok(entry) => {
-                    let file_type = entry.file_type().unwrap();
-                    if !file_type.is_dir() {
-                        let path = entry.path().to_path_buf();
-                        match search_file(path, &matcher, &mut searcher) {
-                            Ok(file_results) => {
-                                if !file_results.is_empty() {
-                                    queue.push(file_results);
-                                }
-                            }
-                            Err(_err) => (),
-                        }
-                    }
-                    ignore::WalkState::Continue
+            match search_reader(buf.as_slice(), &matcher, &mut searcher) {
+                Ok(search_results) => {
+                    let printer_config = PrinterConfig {
+                        mode: cli_args.print_mode,
+                        absolute_paths: cli_args.absolute_paths,
+                        colored_output: cli_args.colored_output,
+                        disable_hyperlinks: cli_args.disable_hyperlinks,
+                        ..Default::default()
+                    };
+                    let mut printer = Printer::new(printer_config);
+                    printer.write(FileResults {
+                        path: PathBuf::from("stdin"),
+                        results: search_results,
+                    })?;
+                    printer.print()?;
                 }
                 Err(err) => {
                     eprintln!("Error: {}", err);
-                    ignore::WalkState::Continue
                 }
-            })
-        });
-
-        let printer_config = PrinterConfig {
-            mode: cli_args.print_mode,
-            absolute_paths: cli_args.absolute_paths,
-            colored_output: cli_args.colored_output,
-            disable_hyperlinks: cli_args.disable_hyperlinks,
-            ..Default::default()
-        };
-        let mut printer = Printer::new(printer_config);
-        let printer_queue = Arc::into_inner(queue).unwrap();
-        printer_queue
-            .into_iter()
-            .for_each(|file_results| printer.write(file_results).unwrap());
-
-        printer.print()?;
-        Ok(())
+            }
+            return Ok(());
+        }
     }
+
+    let queue: Arc<SegQueue<FileResults>> = Arc::new(SegQueue::new());
+
+    let haystack_builder = walk_builder(
+        cli_args.paths.iter().map(|p| p.as_path()).collect(),
+        &cli_args.ignored_paths,
+        cli_args.n_threads,
+        !cli_args.disregard_gitignore,
+        cli_args.filter_filetypes,
+    );
+    let matcher: Arc<RegexMatcher> = Arc::new(build_matcher(&cli_args.patterns)?);
+    haystack_builder.build_parallel().run(|| {
+        let matcher = Arc::clone(&matcher);
+        let mut searcher = build_searcher(cli_args.multiline);
+        let queue = Arc::clone(&queue);
+        Box::new(move |entry: Result<DirEntry, ignore::Error>| match entry {
+            Ok(entry) => {
+                let file_type = entry.file_type().unwrap();
+                if !file_type.is_dir() {
+                    let path = entry.path().to_path_buf();
+                    match search_file(path, &matcher, &mut searcher) {
+                        Ok(file_results) => {
+                            if !file_results.is_empty() {
+                                queue.push(file_results);
+                            }
+                        }
+                        Err(_err) => (),
+                    }
+                }
+                ignore::WalkState::Continue
+            }
+            Err(err) => {
+                eprintln!("Error: {}", err);
+                ignore::WalkState::Continue
+            }
+        })
+    });
+
+    let printer_config = PrinterConfig {
+        mode: cli_args.print_mode,
+        absolute_paths: cli_args.absolute_paths,
+        colored_output: cli_args.colored_output,
+        disable_hyperlinks: cli_args.disable_hyperlinks,
+        ..Default::default()
+    };
+    let mut printer = Printer::new(printer_config);
+    let printer_queue = Arc::into_inner(queue).unwrap();
+    printer_queue
+        .into_iter()
+        .for_each(|file_results| printer.write(file_results).unwrap());
+
+    printer.print()?;
+    Ok(())
 }
